703fd5068dafea0aa96d3e7155deb082b89111f7
heartbeatLV <lscorionjs@gmail.com>
2022-03-29 14:28:32 +0200

    update roadmap

--- src/pages/roadmap.vue
+++ src/pages/roadmap.vue
@@ -1,7 +1,7 @@
 <template>
   <div m2>
     <div mb2 text-3xl underline>ROADMAP</div>
-    <ul ml3 list-disc>
+    <ul ml4 list-disc>
       <li line-through>Impostazione layout base</li>
       <li>
         Autenticazione
@@ -14,18 +14,14 @@
       <li line-through>
         Lista utenti online
         <ul ml4 list-disc>
-          <li line-through>Trigger/Function PgSql</li>
-          <li line-through>Realtime su <n-button type="primary" tag="a" underline text href="https://developer.mozilla.org/en-US/docs/Web/API/Document/visibilityState">document.visibilityState</n-button></li>
+          <li>Trigger/Function PgSql</li>
+          <li>Realtime su <n-button type="primary" tag="a" underline text href="https://developer.mozilla.org/en-US/docs/Web/API/Document/visibilityState">document.visibilityState</n-button></li>
         </ul>
       </li>
-      <li line-through>
-        Lobby
-        <ul ml4 list-disc>
-          <li line-through>Gestione lobby</li>
-          <li line-through>Gestione inviti</li>
-        </ul>
-      </li>
-      <li>Inserire audio Ronaldo quando si verifica Sium</li>
+      <li line-through>Gestione lobby</li>
+      <li line-through>Inserire audio Ronaldo quando si verifica Sium</li>
+      <li>Gestione inviti</li>
+      <li>Gestione turni</li>
       <li>NON CLICCARE SULLA SCRITTA SIUM</li>
     </ul>
   </div>
