942cbdd269cb75ad8195fd6c2e0cba88efddd820
heartbeatLV <lscorionjs@gmail.com>
2022-03-30 17:39:00 +0200

    fix

--- src/composables/results.ts
+++ src/composables/results.ts
@@ -3,7 +3,7 @@ import type { Result } from '~/types'
 export const result = ref<Result>([])
 
 const sum = computed(() => result.value.reduce((t, val) => t + val, 0))
-const sorted = computed(() => [...new Set(result.value)].sort((a, b) => a - b).join(''))
+export const sorted = computed(() => [...new Set(result.value)].sort((a, b) => a - b).join(''))
 const counters = computed(() =>
   result.value.reduce((res, face) => {
     res[face - 1]++
@@ -22,7 +22,7 @@ export const results = computed(() => ({
   tris: counters.value.some(v => v > 2) ? sum.value : 0,
   poker: counters.value.some(v => v > 3) ? sum.value : 0,
   full: (counters.value.includes(3) && counters.value.includes(2)) ? 25 : 0,
-  smallStair: ['12345', '23456', '1234', '2345', '3456'].includes(sorted.value) ? 30 : 0,
+  smallStair: ['1234', '12345', '12346', '2345', '23456', '3456'].includes(sorted.value) ? 30 : 0,
   bigStair: ['12345', '23456'].includes(sorted.value) ? 40 : 0,
   sium: (result.value.length === 5 && counters.value.filter(v => v === 0).length === 5) ? 50 : 0,
   sum: sum.value,
